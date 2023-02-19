f6291ce05a23dae836f83cfc0a034d69691b461d
Federico <isma-fb@hotmail.com>
2023-02-19 20:46:27 +0100

    notes database working - CRUC ready

--- src/index.js
+++ src/index.js
@@ -3,6 +3,8 @@ const path = require('path')
 const { engine } = require('express-handlebars');
 const methodOverride = require('method-override');
 const session = require('express-session');
+const flash = require('connect-flash');
+const { request } = require('http');
 
 
 //initialization
@@ -16,21 +18,33 @@ app.engine('.hbs', engine({
     defaultLayout: 'main',
     layoutDir: path.join(app.get('views'), 'layouts'),
     partialsDir: path.join(app.get('views'), 'partials'),
-    extname: '.hbs'
+    extname: '.hbs',
+    helpers: require('./lib/handlebars'),
+    //handlerbar action pass like object
+    runtimeOptions: {
+      allowProtoPropertiesByDefault: true,
+      allowProtoMethodsByDefault: true
+    }
 }));
 app.set('view engine', '.hbs')//configura el motor de las vistas con .hbs
 
 //midlewares
 app.use(express.urlencoded({extended: false}))//method to stand data 
-app.use(methodOverride('_methods')); //sirve para que los formularios puedan enviar otros metodos como PUT y DELETE
+app.use(methodOverride('_method')); //sirve para que los formularios puedan enviar otros metodos como PUT y DELETE
 app.use(session({
     secret: 'potatoes',
     resave: true,
     saveUninitialized: true
 })); //permite autenticar al usuario y almacenar temporalmente
+app.use(flash());
 
 //gobal variables
+app.use((req, res, next) => {
+    res.locals.success_msg = req.flash('success_msg');
+    res.locals.errors_msg = req.flash('errors_msg');
 
+    next();
+});
 
 //routes
 app.use(require('./routes/index'));
--- src/routes/notes.js
+++ src/routes/notes.js
@@ -1,9 +1,59 @@
 const express = require('express');
 const router = express.Router();
 
-router.get('/notes', (req,res) => {
-    res.send('notes DB')
-})
+const Note = require('../models/Note');
 
+router.get('/notes/add', (req,res) => {
+    res.render('notes/new_note')
+});
+
+router.post('/notes/new_note', async (req,res) => {
+    const { title, description } = req.body
+    const errors = [];
+    if (!title){
+        errors.push({text: 'Por favor ingrese un titulo'})
+    }
+    if(!description){
+        errors.push({text: 'Por favor ingrese una descripción'})
+    }
+    if(errors.length > 0){
+        res.render('notes/new_note',  {
+            errors, 
+            title,
+            description
+        });
+    }else{
+        const newNote = new Note({ title, description });
+        // console.log(newNote);
+        await newNote.save();
+        req.flash('success_msg', 'Note added successfully!');
+        res.redirect('/notes');
+    }
+});
+
+router.get('/notes', async (req,res) => {    
+    const notes = await Note.find().sort({date: 'desc'});
+    res.render('notes/all_notes', { notes });
+});
+
+router.get('/notes/edit/:id', async (req,res) => {
+    const note = await Note.findById(req.params.id)
+    res.render('notes/edit_note', { note });
+});
+
+router.put('/notes/edit_note/:id', async (req, res) => {
+    const { title, description } = req.body
+    // this method find id and update the date{} 
+    await Note.findByIdAndUpdate(req.params.id, { title, description });
+    req.flash('success_msg', 'Note updated successfully!');
+    res.redirect('/notes');
+});
+
+router.delete('/notes/delete/:id', async (req, res) => {
+    await Note.findByIdAndDelete(req.params.id);
+    req.flash('success_msg', 'Note deleted successfully!');
+    res.redirect('/notes');
+    
+});
 
 module.exports = router;
